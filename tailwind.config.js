f50cf15b47d18fd77018ac80579ee1e607f6b820
armanx0333 <armansethi64@gmail.com>
2024-03-22 23:17:41 +0530

    basic age calculator

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nanum-brush-script': ['Nanum Brush Script', 'cursive'],
+        'pacifico': ['Pacifico', 'cursive'],
+        'whisper': ['Whisper', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
